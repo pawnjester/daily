82f799bf590ba7d8e574dfafbae1fee1638a6470
Emmanuel Okonji <emmanuel_andela@medable.com>
2019-11-15 12:29:44 +0100

    remove-unwanted-code

--- lib/views/LoginPage.dart
+++ lib/views/LoginPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:daily_app/services/authentication.dart';
-import 'package:logger/logger.dart';
 
 class LoginPage extends StatefulWidget {
   final BaseAuth auth;
@@ -52,8 +51,6 @@ class _LoginPageState extends State<LoginPage> {
             widget.loginCallback();
           }
         } catch (e) {
-          final logger = Logger();
-          logger.e(e);
           Scaffold.of(_scaffoldContext).showSnackBar(SnackBar(
             content: Text("The email/password is invalid"),
             duration: Duration(seconds: 5),
